615bb8eb701d35832e28d672906060cd5279c75a
cbb <469138776@qq.com>
2020-04-24 09:23:16 +0800

    tijiao1

--- day04_eesy_02cache/src/test/java/com/xkz/test/AccountTest.java
+++ day04_eesy_02cache/src/test/java/com/xkz/test/AccountTest.java
@@ -1,7 +1,7 @@
 package com.xkz.test;
 
-import com.xkz.dao.UserDao;
-import com.xkz.domain.User;
+import com.xkz.dao.AccountDao;
+import com.xkz.domain.Account;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,10 +13,10 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.List;
 
-public class UserTest {
+public class AccountTest {
     private InputStream in;
     private SqlSession sqlSession;
-    private UserDao userDao;
+    private AccountDao accountDao;
 
     @Before//用于在测试方法执行之前执行
     public void init() throws Exception {
@@ -27,7 +27,7 @@ public class UserTest {
         //3.获取SqlSession对象
         sqlSession = factory.openSession(true);
         //4.获取dao的代理对象
-        userDao = sqlSession.getMapper(UserDao.class);
+        accountDao = sqlSession.getMapper(AccountDao.class);
     }
 
     @After//用于在测试方法执行之后执行
@@ -44,14 +44,13 @@ public class UserTest {
      */
     @Test
     public void testFindAll(){
-        List<User> users = userDao.findAll();
-        /*for (User user :
-                users) {
-            System.out.println("-------每个用户的信息-------");
-            System.out.println(user);
-            System.out.println(user.getAccounts());
+        List<Account> accounts = accountDao.findAll();
+        /*for (Account account: accounts) {
+            System.out.println("------------每个account的信息----------------");
+            System.out.println(account);
+            System.out.println(account.getUser());
+
         }*/
     }
-
 
 }
---
+++ day04_eesy_02cache/src/test/java/com/xkz/test/MybatisTest.java
@@ -0,0 +1,135 @@
+package com.xkz.test;
+
+import com.xkz.dao.UserDao;
+import com.xkz.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisTest {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private UserDao userDao;
+
+    @Before//用于在测试方法执行之前执行
+    public void init() throws Exception {
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession(true);
+        //4.获取dao的代理对象
+        userDao = sqlSession.getMapper(UserDao.class);
+    }
+
+    @After//用于在测试方法执行之后执行
+    public void destroy() throws Exception {
+        //提交事务
+        //sqlSession.commit();
+        //6.释放资源
+        sqlSession.close();
+        in.close();
+    }
+
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindAll() {
+        //5.执行查询所有方法
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+
+
+    /**
+     * 测试查询一个操作
+     */
+    @Test
+    public void testFindOne() {
+
+        //5.执行查询一个方法
+        User user = userDao.findById(3);
+        System.out.println(user);
+    }
+
+    /**
+     * 测试模糊查询操作
+     *//*
+    @Test
+    public void testFindByName() {
+
+        //5.执行查询一个方法
+        //List<User> users = userDao.findByName("%王%");
+        List<User> users = userDao.findByName("王");
+        for (User user :
+                users) {
+            System.out.println(user);
+        }
+    }
+
+
+    *//**
+     * 测试使用queryVo作为查询条件
+     *//*
+    @Test
+    public void testFindByVo() {
+        QueryVo vo = new QueryVo();
+        User user = new User();
+        user.setUserName("%王%");
+        vo.setUser(user);
+        //5.执行查询一个方法
+        //List<User> users = userDao.findByName("%王%");
+        List<User> users = userDao.findUserByVo(vo);
+        for (User u : users) {
+            System.out.println(u);
+        }
+    }
+
+    *//**
+     * 测试查询所有
+     *//*
+    @Test
+    public void testFindByCondition() {
+        User u = new User();
+        u.setUserName("老王");
+        //5.执行查询所有方法
+        List<User> users = userDao.findUserByCondition(u);
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+
+    *//**
+     * 测试foreache标签的使用
+     *//*
+    @Test
+    public void testFindInIds() {
+        QueryVo vo = new QueryVo();
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        vo.setIds(list);
+        //5.执行查询所有方法
+        List<User> users = userDao.findUserInIds(vo);
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+*/
+}
