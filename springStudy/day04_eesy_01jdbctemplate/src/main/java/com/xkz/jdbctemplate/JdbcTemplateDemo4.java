22c5c18f07a6592682f6ad4339257d50dbf55f22
cbb <469138776@qq.com>
2020-05-05 18:01:09 +0800

    五月五日的提交

---
+++ springStudy/day04_eesy_01jdbctemplate/src/main/java/com/xkz/jdbctemplate/JdbcTemplateDemo4.java
@@ -0,0 +1,26 @@
+package com.xkz.jdbctemplate;
+
+import com.xkz.dao.IAccountDao;
+import com.xkz.domain.Account;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * JdbcTemplate的最基本用法
+ */
+public class JdbcTemplateDemo4 {
+    public static void main(String[] args) {
+        //1.获取容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2.获取对象
+        IAccountDao accountDao = ac.getBean("accountDao", IAccountDao.class);
+
+        Account account = accountDao.findAccountById(1);
+        System.out.println(account);
+
+        account.setMoney(10000f);
+        accountDao.updateAccount(account);
+
+    }
+}
