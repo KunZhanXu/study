907d2a1312819948454fa7c907c63fd4a3114e52
xukunzhan <1755342479@qq.com>
2020-05-06 23:00:24 +0800

    ga

---
+++ springmvcstudy/springmvc_day01_01_start/src/main/java/cn/xkz/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.xkz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//控制器
+@Controller
+public class HelloController {
+
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("hello springmvc");
+        return null;
+    }
+}
